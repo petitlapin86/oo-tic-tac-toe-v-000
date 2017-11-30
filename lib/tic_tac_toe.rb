d9d1a0aed832b8e7d748066d1cb92e275027c8e1
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-30 18:54:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,4 +42,9 @@ class TicTacToe
        @board[index]== "X" || @board[index] == "O" || @board[index] !== " "
     end
 
+    def valid_move?(board, index)
+      index.between?(0,8) && !position_taken?(board, index)
+    end
+
+
   end
