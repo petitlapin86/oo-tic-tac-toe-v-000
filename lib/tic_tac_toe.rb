da32e69581703e864997f21d35491dd73e3e0218
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-30 18:36:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,6 +11,17 @@ class TicTacToe
       @board.count{|token| token == "X" || token == "O"}
     end
 
+    WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
     def display_board
       puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
       puts "-----------"
@@ -18,4 +29,8 @@ class TicTacToe
       puts "-----------"
       puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
     end
+
+
+
+  
   end
