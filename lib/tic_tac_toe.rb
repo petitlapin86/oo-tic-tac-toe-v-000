8d272c6cf9c5afd6b934ab9db95eda0493f471d3
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-30 18:50:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ class TicTacToe
     end
 
     def position_taken?(@board, index)
-  @board[index]== "X" || @board[index] == "O"
+  @board[index]== "X" || @board[index] == "O" || @board[index] == " "
 end
 
   end
